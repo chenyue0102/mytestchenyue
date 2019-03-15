fbe38601cc14762e95a52199f23bdb24554543e5
chenyue <chenyue@ksjgs.com>
2019-03-15 11:20:17 +0800

    test

---
+++ TestProject/TestDosDShow/DShowHelper.cpp
@@ -0,0 +1,217 @@
+#include "DShowHelper.h"
+#include <assert.h>
+#include <atlbase.h>
+#pragma comment(lib, "Strmiids.lib")
+
+#define FRIENDLY_NAME L"FriendlyName"
+namespace DShowHelper
+{
+HRESULT EnumerateDevices(REFGUID category, IEnumMoniker ** ppEnum)
+{
+	HRESULT hr = S_FALSE;
+
+	do
+	{
+		if (nullptr == ppEnum)
+		{
+			assert(false);
+			break;
+		}
+		CComPtr<ICreateDevEnum> pDevEnum;
+		// Create the System Device Enumerator.
+		hr = CoCreateInstance(CLSID_SystemDeviceEnum, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pDevEnum));
+		if (FAILED(hr))
+		{
+			assert(false);
+			break;
+		}
+		// Create an enumerator for the category.
+		hr = pDevEnum->CreateClassEnumerator(category, ppEnum, 0);
+		if (hr == S_FALSE)
+		{
+			hr = VFW_E_NOT_FOUND;
+		}
+	} while (false);
+	
+	return hr;
+}
+void GetFriendlyNames(REFGUID category, std::vector<std::wstring>& friendlyNames)
+{
+	CComPtr<IEnumMoniker> pEnum;
+	if (FAILED(EnumerateDevices(category, &pEnum)))
+	{
+		assert(false);
+		return;
+	}
+	pEnum->Reset();
+	CComPtr<IMoniker> pMoniker;
+	while (pEnum->Next(1, &pMoniker, nullptr) == S_OK)
+	{
+		CComPtr<IPropertyBag> pPropBag;
+		HRESULT hr = pMoniker->BindToStorage(0, 0, IID_PPV_ARGS(&pPropBag));
+		pMoniker = NULL;
+		if (FAILED(hr))
+		{
+			assert(false);
+			continue;
+		}
+		CComVariant var;
+		hr = pPropBag->Read(FRIENDLY_NAME, &var, 0);
+		if (FAILED(hr)
+			|| NULL == var.bstrVal)
+		{
+			assert(false);
+			continue;
+		}
+		friendlyNames.push_back(std::wstring(var.bstrVal));
+	}
+}
+HRESULT BindFilter(REFGUID category, LPCTSTR lpFriendlyName, IBaseFilter ** ppFilter)
+{
+
+	if (nullptr == lpFriendlyName
+		|| nullptr == ppFilter)
+	{
+		assert(false);
+		return S_FALSE;
+	}
+	CComPtr<IEnumMoniker> pEnum;
+	if (FAILED(EnumerateDevices(category, &pEnum)))
+	{
+		assert(false);
+		return S_FALSE;
+	}
+	pEnum->Reset();
+	for (CComPtr<IMoniker> pMoniker;pEnum->Next(1, &pMoniker, nullptr) == S_OK; pMoniker = nullptr)
+	{
+		CComPtr<IPropertyBag> pPropBag;
+		HRESULT hr = pMoniker->BindToStorage(0, 0, IID_PPV_ARGS(&pPropBag));
+		if (FAILED(hr))
+		{
+			assert(false);
+			continue;
+		}
+		CComVariant var;
+		hr = pPropBag->Read(FRIENDLY_NAME, &var, 0);
+		if (FAILED(hr)
+			|| NULL == var.bstrVal)
+		{
+			assert(false);
+			continue;
+		}
+		if (0 == wcscmp(var.bstrVal, lpFriendlyName))
+		{
+			return pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)ppFilter);
+		}
+	}
+	return S_FALSE;
+}
+HRESULT AddFilterByCLSID(IGraphBuilder * pGraph, const GUID & clsid, LPCWSTR wszName, IBaseFilter ** ppF)
+{
+	HRESULT hr = S_FALSE;
+
+do
+{
+	if (NULL == pGraph
+		|| NULL == wszName
+		|| NULL == ppF)
+	{
+		assert(false);
+		break;
+	}
+	CComPtr<IBaseFilter> pFilter;
+	if (FAILED(hr = CoCreateInstance(clsid, NULL, CLSCTX_INPROC_SERVER, IID_IBaseFilter, (void**)&pFilter)))
+	{
+		assert(false);
+		break;
+	}
+	if (FAILED(hr = pGraph->AddFilter(pFilter, wszName)))
+	{
+		assert(false);
+		break;
+	}
+	*ppF = pFilter.Detach();
+	hr = S_OK;
+} while (false);
+
+return hr;
+}
+HRESULT GetPin(IBaseFilter * pFilter, PIN_DIRECTION dirrequired, int iNum, IPin ** ppPin)
+{
+	HRESULT hr = S_FALSE;
+
+	do
+	{
+		if (nullptr == pFilter
+			|| nullptr == ppPin)
+		{
+			assert(false);
+			break;
+		}
+		*ppPin = nullptr;
+		CComPtr<IEnumPins> pEnumPins;
+		if (FAILED(hr = pFilter->EnumPins(&pEnumPins)))
+		{
+			assert(false);
+			break;
+		}
+		pEnumPins->Reset();
+		IPin *pPin = nullptr;
+		while (pEnumPins->Next(1, &pPin, nullptr) == S_OK)
+		{
+			PIN_DIRECTION pindir = (PIN_DIRECTION)3;
+			if (SUCCEEDED(pPin->QueryDirection(&pindir))
+				&& pindir == dirrequired)
+			{
+				if (0 == iNum)
+				{
+					*ppPin = pPin;
+					break;
+				}
+				--iNum;
+			}
+			pPin->Release();
+			pPin = nullptr;
+		}
+		hr = (nullptr == *ppPin) ? S_FALSE : S_OK;
+	} while (false);
+
+	return hr;
+}
+HRESULT ConnectFilters(IGraphBuilder * pGraph, IBaseFilter * pSrc, IBaseFilter * pDest)
+{
+	HRESULT hr = S_FALSE;
+
+	do
+	{
+		if (nullptr == pGraph
+			|| nullptr == pSrc
+			|| nullptr == pDest)
+		{
+			assert(false);
+			break;
+		}
+		CComPtr<IPin> pPinIn, pPinOut;
+		if (FAILED(hr = GetPin(pSrc, PINDIR_OUTPUT, 0, &pPinOut)))
+		{
+			assert(false);
+			break;
+		}
+		if (FAILED(hr = GetPin(pDest, PINDIR_INPUT, 0, &pPinIn)))
+		{
+			assert(false);
+			break;
+		}
+		if (FAILED(hr = pGraph->Connect(pPinOut, pPinIn)))
+		{
+			assert(false);
+			break;
+		}
+		hr = S_OK;
+	} while (false);
+	return hr;
+}
+
+};
+
+
---
+++ TestProject/TestDosDShow/TestDosDShow.cpp
@@ -0,0 +1,306 @@
+// TestDosDShow.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <assert.h>
+#include <objbase.h>
+#include <atlbase.h>
+#include <qedit.h>
+#include <strmif.h>
+#include <atomic>
+#include "DShowHelper.h"
+
+bool SaveBitmap(const char *pFileName, int width, int height, int biBitCount, const void *pBuf, int nBufLen)
+{
+	if (nullptr == pFileName
+		|| 0 == width
+		|| 0 == height
+		|| !(biBitCount == 24 || biBitCount == 32)
+		|| nullptr == pBuf
+		|| nBufLen < ((width * height * biBitCount) / 8)
+		)
+	{
+		return false;
+	}
+	else
+	{
+		BITMAPFILEHEADER bfh = { 0 };
+		bfh.bfType = 'MB';
+		bfh.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + nBufLen;
+		bfh.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+		FILE *fp = fopen(pFileName, "wb");
+		fwrite(&bfh, 1, sizeof(bfh), fp);
+
+		BITMAPINFOHEADER bih = { 0 };
+		bih.biSize = sizeof(bih);
+		bih.biWidth = width;
+		bih.biHeight = height;
+		bih.biPlanes = 1;
+		bih.biBitCount = biBitCount;
+		fwrite(&bih, 1, sizeof(bih), fp);
+
+		fwrite(pBuf, 1, nBufLen, fp);
+
+		fclose(fp);
+		return true;
+	}
+}
+
+class CSampleGrabberCB : public ISampleGrabberCB
+{
+public:
+	CSampleGrabberCB()
+		: m_nRef(0)
+	{
+
+	}
+	virtual ~CSampleGrabberCB()
+	{
+
+	}
+public:
+	virtual HRESULT STDMETHODCALLTYPE QueryInterface(
+		/* [in] */ REFIID riid,
+		/* [iid_is][out] */ _COM_Outptr_ void __RPC_FAR *__RPC_FAR *ppvObject)override
+	{
+		return E_NOTIMPL;
+	}
+
+	virtual ULONG STDMETHODCALLTYPE AddRef(void)override
+	{
+		return ++m_nRef;
+	}
+
+	virtual ULONG STDMETHODCALLTYPE Release(void)override
+	{
+		ULONG nRef = --m_nRef;
+		if (0 == nRef)
+		{
+			delete this;
+		}
+		return nRef;
+	}
+public:
+	virtual HRESULT STDMETHODCALLTYPE SampleCB(
+		double SampleTime,
+		IMediaSample *pSample)override
+	{
+		return S_OK;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE BufferCB(
+		double SampleTime,
+		BYTE *pBuffer,
+		long BufferLen)override
+	{
+		SaveBitmap("d:/1.bmp", 1920, 1080, 24, pBuffer, BufferLen);
+		if (bvideo)
+		{
+			
+			return S_OK;
+		}
+		return S_OK;
+	}
+private:
+	std::atomic_ulong m_nRef;
+public:
+	bool bvideo = false;
+};
+HRESULT hr = S_OK;
+void testAudio()
+{
+	CComPtr<ICaptureGraphBuilder2> pCaptureGraphBuilder2;
+	hr = CoCreateInstance(CLSID_CaptureGraphBuilder2, NULL, CLSCTX_INPROC_SERVER, IID_ICaptureGraphBuilder2, (void**)&pCaptureGraphBuilder2);
+	CComPtr<IGraphBuilder> pGraphBuilder;
+	hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC, IID_IGraphBuilder, (void**)&pGraphBuilder);
+	hr = pCaptureGraphBuilder2->SetFiltergraph(pGraphBuilder);
+
+	//获取音频输入
+	std::vector<std::wstring> friendlyNames;
+	DShowHelper::GetFriendlyNames(CLSID_AudioInputDeviceCategory, friendlyNames);
+	CComPtr<IBaseFilter> pDeviceFilter;
+	hr = DShowHelper::BindFilter(CLSID_AudioInputDeviceCategory, friendlyNames[0].c_str(), &pDeviceFilter);
+	hr = pGraphBuilder->AddFilter(pDeviceFilter, L"Audio Capture");
+
+	//音频编码
+	std::vector<std::wstring> friendlyNamesLegacyAmFilterCategory;
+	DShowHelper::GetFriendlyNames(CLSID_LegacyAmFilterCategory, friendlyNamesLegacyAmFilterCategory);
+	CComPtr<IBaseFilter> pMpeg2Filter;
+	hr = DShowHelper::BindFilter(CLSID_LegacyAmFilterCategory, L"Microsoft MPEG-2 Audio Encoder", &pMpeg2Filter);
+	hr = pGraphBuilder->AddFilter(pMpeg2Filter, L"Microsoft MPEG-2 Audio Encoder");
+
+	/*
+	CComPtr<IBaseFilter> pMp3Filter;
+	hr = DShowHelper::BindFilter(CLSID_AudioCompressorCategory, L"MPEG Layer-3", &pMp3Filter);
+	hr = pGraphBuilder->AddFilter(pMp3Filter, L"MPEG Layer-3");
+	hr = DShowHelper::ConnectFilters(pGraphBuilder, pDeviceFilter, pMp3Filter);
+	*/
+	//音频输出到文件
+	CComPtr<IBaseFilter> pFilewriterFilter;
+	hr = DShowHelper::BindFilter(CLSID_LegacyAmFilterCategory, L"File writer", &pFilewriterFilter);
+	CComQIPtr<IFileSinkFilter, &IID_IFileSinkFilter> pFileSinkFilter(pFilewriterFilter);
+	hr = pFileSinkFilter->SetFileName(L"d:/test.mp2", nullptr);
+	hr = pGraphBuilder->AddFilter(pFilewriterFilter, L"File writer");
+
+	//获取音频数据
+	CComPtr<IBaseFilter> pSampleGrabberFilter;
+	hr = CoCreateInstance(CLSID_SampleGrabber, nullptr, CLSCTX_INPROC_SERVER, IID_IBaseFilter, (void**)&pSampleGrabberFilter);
+	CComQIPtr<ISampleGrabber, &IID_ISampleGrabber> pSampleGrabber(pSampleGrabberFilter);
+	CComPtr<ISampleGrabberCB> pSampleGrabberCB(new CSampleGrabberCB);
+	hr = pSampleGrabber->SetCallback(pSampleGrabberCB, 1);
+	hr = pGraphBuilder->AddFilter(pSampleGrabberFilter, L"SampleGrabberFilter");
+
+	hr = DShowHelper::ConnectFilters(pGraphBuilder, pDeviceFilter, pMpeg2Filter);
+	hr = DShowHelper::ConnectFilters(pGraphBuilder, pMpeg2Filter, pSampleGrabberFilter);
+	hr = DShowHelper::ConnectFilters(pGraphBuilder, pSampleGrabberFilter, pFilewriterFilter);
+
+	//获取音频输出
+	std::vector<std::wstring> friendlyNamesAudioRendererCategory;
+	DShowHelper::GetFriendlyNames(CLSID_AudioRendererCategory, friendlyNamesAudioRendererCategory);
+
+
+	CComQIPtr<IMediaControl, &IID_IMediaControl> pMediaControl(pGraphBuilder);
+	hr = pMediaControl->Run();
+}
+
+void testVideo()
+{
+	//视频
+	CComPtr<ICaptureGraphBuilder2> pVCaptureGraphBuilder;
+	hr = CoCreateInstance(CLSID_CaptureGraphBuilder2, NULL, CLSCTX_INPROC_SERVER, IID_ICaptureGraphBuilder2, (void**)&pVCaptureGraphBuilder);
+	CComPtr<IGraphBuilder> pVGraphBuilder;
+	hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC, IID_IGraphBuilder, (void**)&pVGraphBuilder);
+	hr = pVCaptureGraphBuilder->SetFiltergraph(pVGraphBuilder);
+
+	std::vector<std::wstring> vfriendlyNames;
+	DShowHelper::GetFriendlyNames(CLSID_VideoInputDeviceCategory, vfriendlyNames);
+	CComPtr<IBaseFilter> pVDeviceFilter;
+	hr = DShowHelper::BindFilter(CLSID_VideoInputDeviceCategory, L"screen-capture-recorder", &pVDeviceFilter);
+	hr = pVGraphBuilder->AddFilter(pVDeviceFilter, L"Video Capture");
+
+	/*CComPtr<IBaseFilter> pVMpeg2Filter;
+	hr = DShowHelper::BindFilter(CLSID_LegacyAmFilterCategory, L"Microsoft MPEG-2 Video Encoder", &pVMpeg2Filter);
+	hr = pVGraphBuilder->AddFilter(pVMpeg2Filter, L"Microsoft MPEG-2 Video Encoder");*/
+
+	HDC hdc = GetDC(NULL);
+	int iBitDepth = GetDeviceCaps(hdc, BITSPIXEL);
+	ReleaseDC(NULL, hdc);
+	AM_MEDIA_TYPE mediaType = { 0 };
+	mediaType.majortype = MEDIATYPE_Video;
+	switch (iBitDepth)
+	{
+	case 8:
+	{
+		mediaType.subtype = MEDIASUBTYPE_RGB8;
+		break;
+	}
+	case 16:
+	{
+		mediaType.subtype = MEDIASUBTYPE_RGB555;
+		break;
+	}
+	case 24:
+	{
+		mediaType.subtype = MEDIASUBTYPE_RGB24;
+		break;
+	}
+	case 32:
+	{
+		mediaType.subtype = MEDIASUBTYPE_RGB32;
+		break;
+	}
+	default:
+	{
+		assert(false);
+		break;
+	}
+	}
+	mediaType.formattype = FORMAT_VideoInfo;
+	CComPtr<IBaseFilter> pVSampleGrabberFilter;
+	hr = CoCreateInstance(CLSID_SampleGrabber, nullptr, CLSCTX_INPROC_SERVER, IID_IBaseFilter, (void**)&pVSampleGrabberFilter);
+	CComQIPtr<ISampleGrabber, &IID_ISampleGrabber> pVSampleGrabber(pVSampleGrabberFilter);
+	CComPtr<CSampleGrabberCB> pVSampleGrabberCB(new CSampleGrabberCB);
+	pVSampleGrabberCB->bvideo = true;
+	hr = pVSampleGrabber->SetCallback(pVSampleGrabberCB, 1);
+	hr = pVGraphBuilder->AddFilter(pVSampleGrabberFilter, L"VSampleGrabberFilter");
+
+
+	hr = pVSampleGrabber->SetMediaType(&mediaType);
+	hr = pVCaptureGraphBuilder->RenderStream(&PIN_CATEGORY_PREVIEW, &MEDIATYPE_Video, pVDeviceFilter, pVSampleGrabberFilter, NULL);
+	hr = pVSampleGrabber->GetConnectedMediaType(&mediaType);
+	VIDEOINFOHEADER *vih = (VIDEOINFOHEADER*)mediaType.pbFormat;
+	int width = vih->bmiHeader.biWidth;
+	int height = vih->bmiHeader.biHeight;
+	hr = pVSampleGrabber->SetOneShot(FALSE);
+	hr = pVSampleGrabber->SetBufferSamples(TRUE);
+
+	CComPtr<IBaseFilter> pVFilewriterFilter;
+	hr = DShowHelper::BindFilter(CLSID_LegacyAmFilterCategory, L"File writer", &pVFilewriterFilter);
+	CComQIPtr<IFileSinkFilter, &IID_IFileSinkFilter> pVFileSinkFilter(pVFilewriterFilter);
+	hr = pVFileSinkFilter->SetFileName(L"d:/test.bmp", nullptr);
+	hr = pVGraphBuilder->AddFilter(pVFilewriterFilter, L"File writer");
+
+	//hr = DShowHelper::ConnectFilters(pVGraphBuilder, pVDeviceFilter, pVSampleGrabberFilter);
+	//hr = DShowHelper::ConnectFilters(pVGraphBuilder, pVSampleGrabberFilter, pVFilewriterFilter);
+
+	CComQIPtr<IVideoWindow, &IID_IVideoWindow> pVVideoWindow(pVGraphBuilder);
+	pVVideoWindow->put_Owner((OAHWND)GetDesktopWindow());
+	pVVideoWindow->put_Width(400);
+	pVVideoWindow->put_Height(400);
+	pVVideoWindow->put_Visible(OATRUE);
+	CComQIPtr<IMediaControl, &IID_IMediaControl> pVMediaControl(pVGraphBuilder);
+	hr = pVMediaControl->Run();
+}
+
+void testCaptureVideo()
+{
+	//视频
+	CComPtr<ICaptureGraphBuilder2> pVCaptureGraphBuilder;
+	hr = CoCreateInstance(CLSID_CaptureGraphBuilder2, NULL, CLSCTX_INPROC_SERVER, IID_ICaptureGraphBuilder2, (void**)&pVCaptureGraphBuilder);
+	CComPtr<IGraphBuilder> pVGraphBuilder;
+	hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC, IID_IGraphBuilder, (void**)&pVGraphBuilder);
+	hr = pVCaptureGraphBuilder->SetFiltergraph(pVGraphBuilder);
+
+	std::vector<std::wstring> vfriendlyNames;
+	DShowHelper::GetFriendlyNames(CLSID_VideoInputDeviceCategory, vfriendlyNames);
+	CComPtr<IBaseFilter> pVDeviceFilter;
+	hr = DShowHelper::BindFilter(CLSID_VideoInputDeviceCategory, L"screen-capture-recorder", &pVDeviceFilter);
+	hr = pVGraphBuilder->AddFilter(pVDeviceFilter, L"Video Capture");
+
+	AM_MEDIA_TYPE mediaType = { 0 };
+	mediaType.majortype = MEDIATYPE_Video;
+	mediaType.subtype = MEDIASUBTYPE_RGB24;
+	//mediaType.formattype = FORMAT_VideoInfo;
+	CComPtr<IBaseFilter> pVSampleGrabberFilter;
+	hr = CoCreateInstance(CLSID_SampleGrabber, nullptr, CLSCTX_INPROC_SERVER, IID_IBaseFilter, (void**)&pVSampleGrabberFilter);
+	CComQIPtr<ISampleGrabber, &IID_ISampleGrabber> pVSampleGrabber(pVSampleGrabberFilter);
+	hr = pVSampleGrabber->SetMediaType(&mediaType);
+	hr = pVSampleGrabber->SetOneShot(TRUE);
+
+	CComPtr<CSampleGrabberCB> pVSampleGrabberCB(new CSampleGrabberCB);
+	pVSampleGrabberCB->bvideo = true;
+	hr = pVSampleGrabber->SetCallback(pVSampleGrabberCB, 1);
+	hr = pVGraphBuilder->AddFilter(pVSampleGrabberFilter, L"VSampleGrabberFilter");
+	
+	hr = DShowHelper::ConnectFilters(pVGraphBuilder, pVDeviceFilter, pVSampleGrabberFilter);
+
+	CComQIPtr<IMediaControl, &IID_IMediaControl> pVMediaControl(pVGraphBuilder);
+	hr = pVMediaControl->Run();
+
+	CComQIPtr< IMediaEvent, &IID_IMediaEvent > pEvent(pVGraphBuilder);
+	long EvCode = 0;
+
+	hr = pEvent->WaitForCompletion(INFINITE, &EvCode);
+}
+
+int main()
+{
+	
+	CoInitialize(nullptr);
+
+	testCaptureVideo();
+
+    return 0;
+}
+
