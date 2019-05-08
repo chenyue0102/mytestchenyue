d9f8b30c9c34f59be6a50c25d7c6bcf82613a64c
chenyue <chenyue@ksjgs.com>
2019-05-08 10:15:03 +0800

    test

--- TestProject/testqtweb/testqtweb.cpp
+++ TestProject/testqtweb/testqtweb.cpp
@@ -1,6 +1,7 @@
 #include "testqtweb.h"
 #include <QWebEngineSettings>
 #include <QFile>
+#include <QMessageBox>
 #include "MyWebChannel.h"
 
 testqtweb::testqtweb(QWidget *parent)
@@ -8,16 +9,56 @@ testqtweb::testqtweb(QWidget *parent)
 {
 	ui.setupUi(this);
 
-	ui.web->load(QUrl("file:///./1.html"));
+	//ui.web->load(QUrl("file:///./1.html"));
+	connect(ui.pushButtonRunScript, SIGNAL(clicked()), SLOT(onRunScript()));
+	connect(ui.pushButtonBack, SIGNAL(clicked()), SLOT(onBack()));
+	connect(ui.web, SIGNAL(urlChanged(const QUrl &)), SLOT(onUrlChanged(const QUrl &)));
+	connect(ui.web, SIGNAL(loadFinished(bool)), SLOT(onLoadFinished(bool)));
+
+	ui.web->load(QUrl("https://tweixin.kaishustory.com/h5/qrcodeLogin/mtest/login"));
 	QWebEnginePage *page = ui.web->page();
 	MyWebChannel *webChannel = new MyWebChannel(this);
-	webChannel->registerObject("webobj", webChannel);
+	webChannel->registerObject("webobj", this);
 	page->setWebChannel(webChannel);
-
-	connect(ui.pushButtonRunScript, SIGNAL(clicked()), SLOT(onRunScript()));
 }
 
 void testqtweb::onRunScript()
 {
 	ui.web->page()->runJavaScript("showAlert('c++')");
+}
+
+void testqtweb::onUrlChanged(const QUrl & url)
+{
+	//urls.push_back(url);
+}
+
+void testqtweb::onLoadFinished(bool b)
+{
+	QUrl url = ui.web->url();
+	urls.push_back(url);
+}
+
+void testqtweb::onBack()
+{
+	bool b = ui.web->page()->action(QWebEnginePage::Back)->isEnabled();
+	if (b)
+	{
+		ui.web->triggerPageAction(QWebEnginePage::Back);
+	}
+	else
+	{
+		close();
+	}
+}
+
+void testqtweb::getAppInfos(const QString & funName)
+{
+	qDebug() << funName;
+	QString str = QStringLiteral("window.HybridFunc.%1('{\"test\":1}\n')").arg(funName);
+	ui.web->page()->runJavaScript(str);
+}
+
+void testqtweb::showMsgBox(const QString &text)
+{
+	QMessageBox::warning(nullptr, u8"ÌבÊ¾", u8"MyWebChannel::showMsgBox " + text);
 }
