2ca1a903333fec50bf6855cf935b1e1a70854ca2
chenyue <chenyue@ksjgs.com>
2019-09-02 18:45:19 +0800

    test

--- TestProject/testqtweb/testqtweb.cpp
+++ TestProject/testqtweb/testqtweb.cpp
@@ -9,13 +9,11 @@ testqtweb::testqtweb(QWidget *parent)
 {
 	ui.setupUi(this);
 
-	//ui.web->load(QUrl("file:///./1.html"));
+	ui.web->load(QUrl("file:///./1.html"));
 	connect(ui.pushButtonRunScript, SIGNAL(clicked()), SLOT(onRunScript()));
 	connect(ui.pushButtonBack, SIGNAL(clicked()), SLOT(onBack()));
-	connect(ui.web, SIGNAL(urlChanged(const QUrl &)), SLOT(onUrlChanged(const QUrl &)));
-	connect(ui.web, SIGNAL(loadFinished(bool)), SLOT(onLoadFinished(bool)));
 
-	ui.web->load(QUrl("https://tweixin.kaishustory.com/h5/qrcodeLogin/mtest/login"));
+	//ui.web->load(QUrl("https://tweixin.kaishustory.com/h5/qrcodeLogin/mtest/login"));
 	QWebEnginePage *page = ui.web->page();
 	MyWebChannel *webChannel = new MyWebChannel(this);
 	webChannel->registerObject("webobj", this);
@@ -25,17 +23,6 @@ testqtweb::testqtweb(QWidget *parent)
 void testqtweb::onRunScript()
 {
 	ui.web->page()->runJavaScript("showAlert('c++')");
-}
-
-void testqtweb::onUrlChanged(const QUrl & url)
-{
-	//urls.push_back(url);
-}
-
-void testqtweb::onLoadFinished(bool b)
-{
-	QUrl url = ui.web->url();
-	urls.push_back(url);
 }
 
 void testqtweb::onBack()
