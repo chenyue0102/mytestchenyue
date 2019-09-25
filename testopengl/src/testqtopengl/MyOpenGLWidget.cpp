4a408cc44833814f81633824555ecb8b012ffb49
chenyue <chenyue@ksjgs.com>
2019-09-25 18:21:23 +0800

    test

--- testopengl/src/testqtopengl/MyOpenGLWidget.cpp
+++ testopengl/src/testqtopengl/MyOpenGLWidget.cpp
@@ -12,24 +12,30 @@ const char *vsrc = GET_STR(
 	attribute vec4 vertexIn;
 	attribute vec2 textureIn;
 	varying vec2 textureOut;
+	uniform mat4 matrix;
 	void main()
 	{
-		gl_Position = vertexIn;
+		gl_Position = matrix * vertexIn;
 		textureOut = textureIn;
 	}
 );
 
 const char *fsrc = GET_STR(
+	out  varying vec4 outColor;
 	varying vec2 textureOut;
 	uniform sampler2D tex_rgb;
 	void main()
 	{
-		vec3 irgb = texture2D(tex_rgb, textureOut).rgb;
-		gl_FragColor = vec4(irgb, 1.0);
+		vec4 clr = texture2D(tex_rgb, textureOut).rgba;
+		/*if (clr.a <= 0.0)*/
+		/*	discard;*/
+		outColor = clr;
 	}
 );
+
 MyOpenGLWidget::MyOpenGLWidget(QWidget *parent)
 	: QOpenGLWidget(parent)
+	, m_texture(QOpenGLTexture::Target2D)
 {
 	QImage img(m_width, m_height, QImage::Format_ARGB32_Premultiplied);
 	img.fill(QColor(0, 0, 0, 0));
@@ -39,11 +45,14 @@ MyOpenGLWidget::MyOpenGLWidget(QWidget *parent)
 
 	QPainter painter;
 	painter.begin(&img);
-	painter.setPen(QColor(255, 0, 0));
-	painter.drawText(QRect(0, 0, m_width, m_height), "hello world", option);
+	painter.setPen(QColor(255, 255, 255, 80));
+	painter.drawText(QRect(0, 0, m_width, m_height / 2), "hello world 0123456789", option);
 	painter.end();
 
 	m_img = img;
+
+	m_timer.setInterval(30);
+	connect(&m_timer, SIGNAL(timeout()), SLOT(onTranslate()));
 }
 
 
@@ -55,6 +64,12 @@ MyOpenGLWidget::~MyOpenGLWidget()
 void MyOpenGLWidget::initializeGL()
 {
 	initializeOpenGLFunctions();
+
+	m_texture.setData(m_img);
+	m_texture.setMinificationFilter(QOpenGLTexture::Linear);
+	m_texture.setMagnificationFilter(QOpenGLTexture::Linear);
+	m_texture.setWrapMode(QOpenGLTexture::Repeat);
+
 	bool ret = false;
 	ret = program.addShaderFromSourceCode(QGLShader::Fragment, fsrc);
 	assert(ret);
@@ -101,16 +116,62 @@ void MyOpenGLWidget::initializeGL()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+
+	m_timer.start();
 }
 
 void MyOpenGLWidget::paintGL()
 {
-	program.bind();
+	//启用混合
+	glEnable(GL_BLEND);
+	//GL_ONE_MINUS_SRC_COLOR
+	//GL_ONE_MINUS_SRC_ALPHA
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	//绘制背景色
+	glPolygonMode(GL_FRONT, GL_FILL);//正面填充
+	//glPolygonMode(GL_BACK, GL_FILL);//正面填充
+	glFrontFace(GL_CCW);//逆时针为正面
+	//绘制多边形
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glBegin(GL_POLYGON);
+	glVertex2f(-1.0f, -1.0f);
+	glVertex2f(1.0f, -1.0f);
+	glVertex2f(1.0f, 1.0f);
+	glVertex2f(-1.0f, 1.0f);
+	glEnd();
+
+	/*glColor3f(0.0f, 1.0f, 0.0f);
+	m_texture.bind();
+	glBegin(GL_TRIANGLE_STRIP);
+	glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0f, -1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex2f(1.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0f, 1.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex2f(1.0f, 1.0f);
+	glEnd();
+	m_texture.release();*/
+
+	QMatrix4x4 m;
+	m.translate(m_translateX, -0.8f, 0.0f);
+	m.scale(0.1, 0.1, 1);
+	program.bind();
+	program.setUniformValue("matrix", m);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, m_tex);
 	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, m_width, m_height, GL_RGBA, GL_UNSIGNED_BYTE, m_img.bits());
 	glUniform1i(m_uni, 0);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 	program.release();
+
+	
+}
+
+void MyOpenGLWidget::onTranslate()
+{
+	m_translateX += 0.003;
+	if (m_translateX >= 1.0)
+	{
+		m_translateX = -1.0f;
+	}
+	update();
 }
--- testopengl/src/testqtopengl/MyOpenGLWidget.h
+++ testopengl/src/testqtopengl/MyOpenGLWidget.h
@@ -3,17 +3,22 @@
 #include <QOpenGLFunctions>
 #include <QGLShaderProgram>
 #include <QImage>
+#include <QOpenGLTexture>
+#include <QTimer>
 
 class MyOpenGLWidget 
 	: public QOpenGLWidget
 	, public QOpenGLFunctions
 {
+	Q_OBJECT
 public:
 	MyOpenGLWidget(QWidget *parent = 0);
 	~MyOpenGLWidget();
 protected:
 	virtual void initializeGL();
 	virtual void paintGL();
+public slots:
+	void onTranslate();
 private:
 	QGLShaderProgram program;
 	GLuint m_uni = 0;
@@ -21,5 +26,8 @@ private:
 	QImage m_img;
 	int m_width = 100;
 	int m_height = 50;
+	QOpenGLTexture m_texture;
+	float m_translateX = -1.0f;
+	QTimer m_timer;
 };
 
