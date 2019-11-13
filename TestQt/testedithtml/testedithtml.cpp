2dc212448e5f7d610f18d7dbd7d86f5984a35d20
chenyue <chenyue@ksjgs.com>
2019-11-13 17:59:26 +0800

    test

--- TestQt/testedithtml/testedithtml.cpp
+++ TestQt/testedithtml/testedithtml.cpp
@@ -7,6 +7,7 @@ testedithtml::testedithtml(QWidget *parent)
 {
 	ui.setupUi(this);
 	connect(ui.pushButtonReload, SIGNAL(clicked()), SLOT(onReload()));
+	connect(ui.pushButtonAdd, SIGNAL(clicked()), SLOT(onAdd()));
 
 	QWebEnginePage *page = ui.webEngineView->page();
 	m_webChannel->registerObject("webobj", this);
@@ -18,6 +19,17 @@ testedithtml::~testedithtml()
 	QWebEnginePage *page = ui.webEngineView->page();
 	page->setWebChannel(nullptr);
 	m_webChannel->deregisterObject(this);
+}
+
+void testedithtml::onAdd()
+{
+	QString str = QStringLiteral("appendChatText(%1,'%2','%3')").arg(1).arg(u8"小马'").arg(u8"得得得");
+	QWebEnginePage *p = ui.webEngineView->page();
+	if (nullptr != p)
+	{
+		//p->runJavaScript(str);
+	}
+	emit notifyAddMsg(1, u8"小马'", u8"得得得");
 }
 
 void testedithtml::onReload()
--- TestQt/testedithtml/testedithtml.h
+++ TestQt/testedithtml/testedithtml.h
@@ -7,12 +7,14 @@
 class testedithtml : public QWidget
 {
 	Q_OBJECT
-
+Q_SIGNALS:
+	void notifyAddMsg(int id, const QString &name, const QString &text);
 public:
 	testedithtml(QWidget *parent = Q_NULLPTR);
 	~testedithtml();
 public slots:
 	void onReload();
+	void onAdd();
 private:
 	Ui::testedithtmlClass ui;
 	QWebChannel *m_webChannel;
