0365c46856db41ac38f68c4ec46ec98d3cfded78
chenyue <chenyue@ksjgs.com>
2019-12-13 17:12:39 +0800

    test

--- python/httpclient.py
+++ python/httpclient.py
@@ -5,14 +5,29 @@ import parser
 import sys
 
 
-def testGET():
+def testGET(paramText):
     con = http.client.HTTPConnection('localhost')
-    con.request('GET', '/getVersion')
-    response = con.getresponse();
+    con.request('GET', paramText)
+    response = con.getresponse()
     print(response.status, response.reason)
     print(response.getheaders())
     print(response.read())
     con.close()
+    return
+
+
+def testDownload(paramText, filePath):
+    con = http.client.HTTPConnection('localhost')
+    con.request('GET', paramText)
+    response = con.getresponse()
+    print(response.status, response.reason)
+    print(response.getheaders())
+    # print(response.read())
+    d = response.read();
+    con.close()
+    f = open(filePath, 'wb')
+    f.write(d)
+    f.close()
     return
 
 
@@ -21,11 +36,21 @@ def testPOST():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 0:
+    print(sys.argv)
+    paramLen = len(sys.argv)
+    paramText = ''
+    paramFilePath = ''
+    if paramLen >= 3:
+        paramText = sys.argv[2]
+    if paramLen >= 4:
+        paramFilePath = sys.argv[3]
+    if paramLen >= 1:
         if sys.argv[1] == '1':
-            testGET()
+            testGET(paramText)
         elif sys.argv[1] == '2':
             testPOST()
+        elif sys.argv[1] == '3':
+            testDownload(paramText, paramFilePath)
         else:
             print('param error')
     else:
--- python/httpserver.py
+++ python/httpserver.py
@@ -1,15 +1,71 @@
 # -*- coding:UTF-8 -*-
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+from urllib import parse
+
+
+testVersion = 'abcd1234'
+
+def getModuleVersion(qs):
+    module = ''
+    version = ''
+    for key, value in qs.items():
+        if key == 'module':
+            if len(value) >= 1:
+                module = value[0]
+        elif key == 'version':
+            if len(value) >= 1:
+                version = value[0]
+
+    print('module=' + module + ' version=' + version)
+    return module, version
 
 
 class Resquest(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        data = {'version': 'abc123'}
-        self.wfile.write(json.dumps(data).encode())
+        data = {}
+        errcode = 404
+        print('path' + str(type(self.path)) + self.path)
+        o = parse.urlparse(self.path)
+        print(
+            'scheme=' + o.scheme + ' netloc=' + o.netloc + ' path=' + o.path + ' query=' + o.query + ' fragment=' + o.fragment)
+        if o.path == '/getVersion':
+            qs = parse.parse_qs(o.query)
+            module, version = getModuleVersion(qs)
+            if module == "test":
+                data['module'] = module;
+                data['version'] = testVersion
+                if version != testVersion:
+                    data['url'] = 'http://localhost/download/test.zip'
+                errcode = 200
+            else:
+                data['module'] = module
+                data['version'] = 'unknown'
+                errcode = 404
+
+            self.send_response(errcode)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+        elif o.path == '/download/test.zip':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/octet-stream')
+            self.end_headers()
+            f = open('./packet.zip', 'rb')
+            while True:
+                d = f.read(1024);
+                if not d:
+                    break
+                self.wfile.write(d)
+            f.close()
+        else:
+            data = {}
+            self.send_response(errcode)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+        return
 
 
 if __name__ == '__main__':
