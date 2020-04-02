1f65c2a7b28b2b1436fcda83cf004265e57edd32
chenyue <chenyue@ksjgs.com>
2020-04-02 14:23:08 +0800

    tet

--- python/qt.py
+++ python/qt.py
@@ -2,25 +2,39 @@
 # -*- coding: UTF-8 -*-
 import sys
 import random
-#from PyQt5.QtWidgets import QApplication, QWidget
-from PySide2 import QtCore, QtWidgets, QtGui
+from PySide2.QtWidgets import (QApplication, QLabel, QPushButton,
+                               QVBoxLayout, QWidget)
+from PySide2.QtCore import Slot, Qt
 
-class MyWidget(QtWidgets.QWidget):
+class MyWidget(QWidget):
     def __init__(self):
-        self.button = QtWidgts.QPushButton("ok")
-        self.text = QWidgets.QLabel("tip")
-        self.layout = QWidgets.QVBoxLayout()
-        self.layout.addQWidget(self.text)
-        self.layout.addQWidget(self.button)
+        QWidget.__init__(self)
+
+        self.hello = ["Hallo Welt", "你好，世界", "Hei maailma",
+            "Hola Mundo", "Привет мир"]
+
+        self.button = QPushButton("Click me!")
+        self.text = QLabel("Hello World")
+        self.text.setAlignment(Qt.AlignCenter)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
         self.setLayout(self.layout)
-        self.button.clicked.connect(self.onclick)
 
-    def onclick(self):
-        self.close()
+        # Connecting the signal
+        self.button.clicked.connect(self.magic)
 
-if __name__ == '__main__':
-    myapp = QApplication(sys.argv)
-    mywidget = MyWidget()
-    mywidget.show()
-    sys.exit(myapp.exec_())
+    @Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
 
