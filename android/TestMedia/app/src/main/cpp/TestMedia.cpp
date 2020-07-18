3a5f6a14740e3ef1771c3b9a6751c1ac2771015f
chenyue0102 <chenyue0102@gmail.com>
2020-07-18 17:45:26 +0800

    test

---
+++ android/TestMedia/app/src/main/cpp/TestMedia.cpp
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+#include <pthread.h>
+#include <errno.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/system_properties.h>
+#include <sys/socket.h>
+#include <endian.h>
+#include <linux/in.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+
---
+++ android/TestMedia/app/src/main/java/com/example/testmedia/TestMedia.java
@@ -0,0 +1,4 @@
+package com.example.testmedia;
+
+public class TestMedia {
+}
