6cc95c17140470cc436a31a0426b71b493a7113d
chenyue <chenyue@ksjgs.com>
2020-07-22 10:02:45 +0800

    test

--- Test/TestConsole/TestConsole.cpp
+++ Test/TestConsole/TestConsole.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
-
+#include <string>
+#include "RingQueue.h"
 
 int main()
 {
+	RingQueue ringQueue(5);
+	size_t dataSize = ringQueue.getDataSize();
+	size_t freeSize = ringQueue.getFreeSize();
+	char  str[] = "123456789";
+	size_t putSize = ringQueue.put(str, 2);
+	char out[12] = { 0 };
+	size_t getSize = ringQueue.get(out, 12);
+	putSize = ringQueue.put(str, sizeof(str));
+	getSize = ringQueue.get(out, 12);
     return 0;
 }
 
