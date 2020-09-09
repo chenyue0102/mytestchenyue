da6778f8e52ae816912c2b3e27161bb4893f2a3a
chenyue <chenyue@ksjgs.com>
2020-09-09 19:03:11 +0800

    test

--- Unity/testUnity/Assets/Scripts/Enemy/HitPoint.cs
+++ Unity/testUnity/Assets/Scripts/Enemy/HitPoint.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class HitPoint : MonoBehaviour
 {
+    public bool bombAvilable;
+    int dir;
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<IDamageable>().GetHit(1);
         }
-        else if (collision.CompareTag("Bomb"))
+        else if (collision.CompareTag("Bomb") && bombAvilable)
         {
-
+            if (transform.position.x > collision.transform.position.x)
+            {
+                dir = -1;
+            }
+            else
+            {
+                dir = 1;
+            }
+            Debug.Log("踢炸弹");
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(dir, 1) * 10, ForceMode2D.Impulse);
         }
     }
 }
