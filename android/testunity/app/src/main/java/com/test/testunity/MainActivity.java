7ce8f57d9a555cede3d2b283af4507159d9d6c5b
chenyue <chenyue@ksjgs.com>
2020-09-11 17:40:16 +0800

    test

---
+++ android/testunity/app/src/main/java/com/test/testunity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.test.testunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_start_game).setOnClickListener(v->startGame());
+    }
+
+    private void startGame(){
+        Intent intent = new Intent(this, UnityPlayerActivity.class);
+        startActivity(intent);
+    }
+}
