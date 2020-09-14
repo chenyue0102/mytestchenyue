084f47c8fa9110dab1d7e2ad596799e55b323ad9
chenyue <chenyue@ksjgs.com>
2020-09-14 14:50:16 +0800

    test

---
+++ TestIOS/testios/testios/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  testios
+//
+//  Created by chenyue on 2020/9/14.
+//  Copyright © 2020 chenyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TestIOS/testios/testiosTests/testiosTests.m
@@ -0,0 +1,37 @@
+//
+//  testiosTests.m
+//  testiosTests
+//
+//  Created by chenyue on 2020/9/14.
+//  Copyright © 2020 chenyue. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testiosTests : XCTestCase
+
+@end
+
+@implementation testiosTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
