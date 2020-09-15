73c0a98871beba90c788f54a2ff1b68644a612c3
chenyue <chenyue@ksjgs.com>
2020-09-15 10:57:50 +0800

    test

---
+++ TestIOS/testios/testios/MyAlertController.h
@@ -0,0 +1,13 @@
+//
+//  MyAlertController.h
+//  testios
+//
+//  Created by chenyue on 2020/9/14.
+//  Copyright © 2020 chenyue. All rights reserved.
+//
+
+#ifndef MyAlertController_h
+#define MyAlertController_h
+
+
+#endif /* MyAlertController_h */
---
+++ TestIOS/testios/testios/MyAlertController.m
@@ -0,0 +1,9 @@
+//
+//  MyAlertController.m
+//  testios
+//
+//  Created by chenyue on 2020/9/14.
+//  Copyright © 2020 chenyue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
