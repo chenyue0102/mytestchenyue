176706269423a6ae36532fd45d95e9c7a0835d0c
chenyue0102 <chenyue0102@gmail.com>
2020-11-13 16:54:52 +0800

    test

--- android/SpineDemo/app/src/main/java/com/ksstory/spinewrap/TextureLoaderJNI.java
+++
@@ -1,6 +0,0 @@
-package com.ksstory.spinewrap;
-
-public class TextureLoaderJNI {
-    public final static native long new_TextureLoader();
-    public final static native void delete_TextureLoader(long swigCPtr);
-}
---
+++ android/testffmpeg/app/src/main/cpp/testffmpeg/Equalizer.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//window_size must pow of 2
+struct _equalizer_t* create_equalizer(double sample_rate, size_t window_size, float *Hz, size_t count);
+
+void delete_equalizer(struct _equalizer_t* p);
+
+#ifdef __cplusplus
+}
+#endif
