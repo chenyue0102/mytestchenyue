8d5dd2c47950614cd89c9cda16353f9c9be3c721
chenyue0102 <chenyue0102@gmail.com>
2021-04-23 09:24:54 +0800

    test

---
+++ webrtc/agc/webrtcdemo.cpp
@@ -0,0 +1,13 @@
+﻿// webrtc.cpp: 定义应用程序的入口点。
+//
+
+#include "webrtcdemo.h"
+#include "modules/audio_processing/gain_control_impl.h"
+using namespace std;
+
+int main()
+{
+	webrtc::GainControlImpl imp;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
