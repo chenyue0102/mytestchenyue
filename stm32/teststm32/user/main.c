46c35b0f2eea6b7d768d96c761ea83245e2b7cc5
chenyue0102 <chenyue0102@gmail.com>
2022-10-03 11:23:40 +0800

    增加lcd控制

--- stm32/teststm32/user/main.c
+++ stm32/teststm32/user/main.c
@@ -89,7 +89,7 @@ static void delayUnit(uint32_t i, uint32_t unit)
 	SysTick->VAL = 0;		//清空计数器
 }
 
-static void delayMs(uint32_t i)
+void delayMs(uint32_t i)
 {
 	delayUnit(i, STCLK_MS_UNIT);
 }
@@ -342,11 +342,321 @@ void USART1_IRQHandler(void)
 	
 	USART1SendData(r);
 }
+#define SPI_SCL GPIO_Pin_13			//时钟
+#define SPI_MOSI GPIO_Pin_15			//主机输出
+#define SPI_SDA 
+//#define SPI_MISO GPIO_
+#define OLED_CS GPIO_Pin_11				//片选信号
+#define OLED_RESET GPIO_Pin_12		//液晶屏复位信号，低电平复位
+#define OLED_DC GPIO_Pin_10			//液晶屏寄存器/数据选择信号，低电平：寄存器，高电平：数据 
+
+#define LED_LIGHT GPIO_Pin_9 		//背光
+
+#define OLED_CMD 0
+#define OLED_DATA 1
+
+//LCD重要参数集
+typedef struct  
+{										    
+	u16 width;			//LCD 宽度
+	u16 height;			//LCD 高度
+	u16 id;				  //LCD ID
+	u8  dir;			  //横屏还是竖屏控制：0，竖屏；1，横屏。	
+	u16	 wramcmd;		//开始写gram指令
+	u16  setxcmd;		//设置x坐标指令
+	u16  setycmd;		//设置y坐标指令	 
+}_lcd_dev; 	
+_lcd_dev lcddev;
+
+#define LCD_W 320
+#define LCD_H 480
+
+//#define OLED_RST_Clr() GPIO_ResetBits(GPIOA, OLED_RESET)
+//#define OLED_RST_Set() GPIO_SetBits(GPIOA, OLED_RESET)
+
+//#define OLED_DC_Clr() GPIO_ResetBits(GPIOA, OLED_DC)
+//#define OLED_DC_Set() GPIO_SetBits(GPIOA, OLED_DC)
+
+//#define OLED_SCLK_Clr() GPIO_ResetBits(GPIOA, SPI_SCL)
+//#define OLED_SCLK_Set() GPIO_SetBits(GPIOA, SPI_SCL)
+
+//#define OLED_SDIN_Clr() GPIO_ResetBits(GPIOA, SPI_SDA)
+//#define OLED_SDIN_Set() GPIO_SetBits(GPIOA, SPI_SDA)
+
+
+void LCD_GPIOInit(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	//配置管脚
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	
+	GPIO_InitStructure.GPIO_Pin = LED_LIGHT | OLED_DC | OLED_CS | SPI_SCL | OLED_RESET | SPI_MOSI;//输出与时钟
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;//GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	/*
+	GPIO_InitStructure.GPIO_Pin = SPI_MISO;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	*/
+}
+
+#define LCD_RST_CLR() GPIO_ResetBits(GPIOB, OLED_RESET)
+#define LCD_RST_SET() GPIO_SetBits(GPIOB, OLED_RESET)
+#define LCD_DC_CLR() GPIO_ResetBits(GPIOB, OLED_DC)
+#define LCD_DC_SET() GPIO_SetBits(GPIOB, OLED_DC)
+#define LCD_CS_CLR() GPIO_ResetBits(GPIOB, OLED_CS)
+#define LCD_CS_SET() GPIO_SetBits(GPIOB, OLED_CS)
+#define SPI_MOSI_SET() GPIO_SetBits(GPIOB, SPI_MOSI)
+#define SPI_MOSI_CLR() GPIO_ResetBits(GPIOB, SPI_MOSI)
+#define SPI_SCLK_CLR() GPIO_ResetBits(GPIOB, SPI_SCL)
+#define SPI_SCLK_SET() GPIO_SetBits(GPIOB, SPI_SCL)
+
+
+void LCD_RESET(void)
+{
+	LCD_RST_CLR();
+	delayMs(100);
+	LCD_RST_SET();
+	delayMs(50);
+}
+
+void SPIv_WriteData(u8 Data)
+{
+	unsigned char i=0;
+	for(i=8;i>0;i--)
+	{
+	  if(Data&0x80)	
+	  SPI_MOSI_SET(); 
+      else SPI_MOSI_CLR();
+	   
+      SPI_SCLK_CLR();       
+      SPI_SCLK_SET();
+      Data<<=1; 
+	}
+}
+
+void LCD_WR_DATA(u8 data)
+{
+	LCD_CS_CLR();
+	LCD_DC_SET();
+	SPIv_WriteData(data);
+	LCD_CS_SET();
+}
+
+void LCD_WR_REG(u8 data)
+{
+	LCD_CS_CLR();
+	LCD_DC_CLR();
+	SPIv_WriteData(data);
+	LCD_CS_SET();
+}
+
+void LCD_WriteReg(u8 reg, u16 regValue)
+{
+	LCD_WR_REG(reg);
+	LCD_WR_DATA(regValue);
+}
+
+void LCD_direction(u8 direction)
+{ 
+			lcddev.setxcmd=0x2A;
+			lcddev.setycmd=0x2B;
+			lcddev.wramcmd=0x2C;
+	switch(direction){		  
+		case 0:						 	 		
+			lcddev.width=LCD_W;
+			lcddev.height=LCD_H;		
+			LCD_WriteReg(0x36,(1<<3)|(0<<6)|(0<<7));//BGR==1,MY==0,MX==0,MV==0
+		break;
+		case 1:
+			lcddev.width=LCD_H;
+			lcddev.height=LCD_W;
+			LCD_WriteReg(0x36,(1<<3)|(0<<7)|(1<<6)|(1<<5));//BGR==1,MY==1,MX==0,MV==1
+		break;
+		case 2:						 	 		
+			lcddev.width=LCD_W;
+			lcddev.height=LCD_H;	
+			LCD_WriteReg(0x36,(1<<3)|(1<<6)|(1<<7));//BGR==1,MY==0,MX==0,MV==0
+		break;
+		case 3:
+			lcddev.width=LCD_H;
+			lcddev.height=LCD_W;
+			LCD_WriteReg(0x36,(1<<3)|(1<<7)|(1<<5));//BGR==1,MY==1,MX==0,MV==1
+		break;	
+		default:break;
+	}		
+}	
+
+void LCD_WriteRAM_Prepare(void)
+{
+	LCD_WR_REG(lcddev.wramcmd);
+}	 
+
+void LCD_SetWindows(u16 xStar, u16 yStar,u16 xEnd,u16 yEnd)
+{	
+	LCD_WR_REG(lcddev.setxcmd);	
+	LCD_WR_DATA(xStar>>8);
+	LCD_WR_DATA(0x00FF&xStar);		
+	LCD_WR_DATA(xEnd>>8);
+	LCD_WR_DATA(0x00FF&xEnd);
+
+	LCD_WR_REG(lcddev.setycmd);	
+	LCD_WR_DATA(yStar>>8);
+	LCD_WR_DATA(0x00FF&yStar);		
+	LCD_WR_DATA(yEnd>>8);
+	LCD_WR_DATA(0x00FF&yEnd);
+
+	LCD_WriteRAM_Prepare();	//	
+}   
+
+#define USE_HORIZONTAL  	 0
+
+void Lcd_WriteData_16Bit(u16 Data)
+{	
+  //18Bit	
+	LCD_WR_DATA((Data>>8)&0xF8);//RED
+	LCD_WR_DATA((Data>>3)&0xFC);//GREEN
+	LCD_WR_DATA(Data<<3);//BLUE
+}
+
+void LCD_Clear(u16 Color)
+{
+  unsigned int i,m;  
+	LCD_SetWindows(0,0,lcddev.width-1,lcddev.height-1);   
+	LCD_CS_CLR();
+	LCD_DC_SET();
+	for(i=0;i<lcddev.height;i++)
+	{
+    for(m=0;m<lcddev.width;m++)
+    {	
+			Lcd_WriteData_16Bit(Color);
+		}
+	}
+	 LCD_CS_SET();
+} 
+
+#define WHITE       0xFFFF
+#define BLACK      	0x0000	  
+#define BLUE       	0x001F  
+#define BRED        0XF81F
+#define GRED 			 	0XFFE0
+#define GBLUE			 	0X07FF
+#define RED         0xF800
+#define MAGENTA     0xF81F
+#define GREEN       0x07E0
+#define CYAN        0x7FFF
+#define YELLOW      0xFFE0
+#define BROWN 			0XBC40 //棕色
+#define BRRED 			0XFC07 //棕红色
+#define GRAY  			0X8430 //灰色
+
+void LCD_Fill(u16 sx,u16 sy,u16 ex,u16 ey,u16 color)
+{  	
+	u16 i,j;			
+	u16 width=ex-sx+1; 		//
+	u16 height=ey-sy+1;		//
+	LCD_SetWindows(sx,sy,ex,ey);//
+	for(i=0;i<height;i++)
+	{
+		for(j=0;j<width;j++)
+		Lcd_WriteData_16Bit(color);	//
+	}
+	LCD_SetWindows(0,0,lcddev.width-1,lcddev.height-1);//
+}
+void mainApp(void);
+
+void HAL_Delay(uint32_t t)
+{
+	delayMs(t);
+}
 /**
   * @brief  Main program.
   * @param  None
   * @retval None
-  */
+  */	
+
+void LCD_Init()
+{
+	LCD_GPIOInit();
+	LCD_RESET();
+	
+	LCD_WR_REG(0XF7);
+	LCD_WR_DATA(0xA9);
+	LCD_WR_DATA(0x51);
+	LCD_WR_DATA(0x2C);
+	LCD_WR_DATA(0x82);
+	LCD_WR_REG(0xC0);
+	LCD_WR_DATA(0x11);
+	LCD_WR_DATA(0x09);
+	LCD_WR_REG(0xC1);
+	LCD_WR_DATA(0x41);
+	LCD_WR_REG(0XC5);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x0A);
+	LCD_WR_DATA(0x80);
+	LCD_WR_REG(0xB1);
+	LCD_WR_DATA(0xB0);
+	LCD_WR_DATA(0x11);
+	LCD_WR_REG(0xB4);
+	LCD_WR_DATA(0x02);
+	LCD_WR_REG(0xB6);
+	LCD_WR_DATA(0x02);
+	LCD_WR_DATA(0x42);
+	LCD_WR_REG(0xB7);
+	LCD_WR_DATA(0xc6);
+	LCD_WR_REG(0xBE);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x04);
+	LCD_WR_REG(0xE9);
+	LCD_WR_DATA(0x00);
+	LCD_WR_REG(0x36);
+	LCD_WR_DATA((1<<3)|(0<<7)|(1<<6)|(1<<5));
+	LCD_WR_REG(0x3A);
+	LCD_WR_DATA(0x66);
+	LCD_WR_REG(0xE0);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x07);
+	LCD_WR_DATA(0x10);
+	LCD_WR_DATA(0x09);
+	LCD_WR_DATA(0x17);
+	LCD_WR_DATA(0x0B);
+	LCD_WR_DATA(0x41);
+	LCD_WR_DATA(0x89);
+	LCD_WR_DATA(0x4B);
+	LCD_WR_DATA(0x0A);
+	LCD_WR_DATA(0x0C);
+	LCD_WR_DATA(0x0E);
+	LCD_WR_DATA(0x18);
+	LCD_WR_DATA(0x1B);
+	LCD_WR_DATA(0x0F);
+	LCD_WR_REG(0XE1);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x17);
+	LCD_WR_DATA(0x1A);
+	LCD_WR_DATA(0x04);
+	LCD_WR_DATA(0x0E);
+	LCD_WR_DATA(0x06);
+	LCD_WR_DATA(0x2F);
+	LCD_WR_DATA(0x45);
+	LCD_WR_DATA(0x43);
+	LCD_WR_DATA(0x02);
+	LCD_WR_DATA(0x0A);
+	LCD_WR_DATA(0x09);
+	LCD_WR_DATA(0x32);
+	LCD_WR_DATA(0x36);
+	LCD_WR_DATA(0x0F);
+	LCD_WR_REG(0x11);
+	delayMs(120);
+	LCD_WR_REG(0x29);
+	
+  LCD_direction(USE_HORIZONTAL);
+	GPIO_SetBits(GPIOB, LED_LIGHT);
+	LCD_Clear(WHITE);
+}
 int main(void)
 {
   /*!< At this stage the microcontroller clock setting is already configured, 
@@ -357,19 +667,23 @@ int main(void)
      */     
 
 	SystemInit();
-	
-	LEDInit();
+	//LEDInit();
 	//BuzzerInit();
 	//KeyInit();
 	//EXTIKeyInit();
 	//DigitalInit();
 	//TIMInit();
-	USARTInit();
+	//USARTInit();
+	//OLED_Init();
+	LCD_Init();
+	
+	//mainApp();
 	
 	int num = 0;
 	bool oldValue = false;
   while (1)
   {
+		LCD_Fill(10,10,50,50,BLUE);
 		//GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
 		//GPIO_WriteBit(GPIOA, GPIO_Pin_1, Bit_RESET);
 		delayMs(50);
