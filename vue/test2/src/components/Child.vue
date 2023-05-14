e00058f975d0b6e8b7cbf45100153727a56cfe35
chenyue0102 <chenyue0102@gmail.com>
2023-05-14 20:37:19 +0800

    test

---
+++ vue/test2/src/components/Child.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {computed, getCurrentInstance, reactive, readonly, ref} from 'vue'
+const name="Child"
+const ctx = getCurrentInstance()
+const props = defineProps({
+    count:Number,
+    add:Function,
+})
+const count2 = computed(()=>{
+    return props.count + 1
+})
+const addNum = ()=>{
+    ctx.emit('add', 10)
+}
+const state = reactive({
+    text:"test"
+})
+const text2 = ref('test')
+setTimeout(()=>{
+    //alert('对不起, 要你久候')
+    state.text = 'demos'
+    text2.value = 'demo2'
+    console.log("timeout")
+    state_readonly.name = "new name"
+}, 2000)
+const state_readonly = readonly({
+    name:'name readonly'
+})
+</script>
+<template>
+    <p>count:{{count}}</p>
+    <p>count2:{{count2}}</p>
+    <p>text:{{state.text}} text2:{{text2}}</p>
+    <p>readonly:{{state_readonly.name}}</p>
+    <button @click="addNum">+</button>
+</template>
+<style scoped>
+</style>
--- vue/test2/src/components/HelloWorld.vue
+++ vue/test2/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
+import Child from './Child.vue'
 
 defineProps({
   msg: String,
@@ -8,16 +9,63 @@ defineProps({
 const count = ref(0)
 const spantext = "mytest_spantext"
 const htmlcode = "<p>test</p>"
-const oncetest = "once text"
+var oncetest = "once text"
+const state = reactive({
+  fe:'vue',
+  farray:[],
+  modeltext:"abc",
+  isRed:false,
+  color:'red',
+  fontsize:''
+})
 function change(){
   count.value++
+  oncetest = 'once text2'
+  state.isRed = !state.isRed
+  state.color = 'blue'
+  state.fontsize = 'big'
 }
-
+function padd(num){
+  count.value += num
+}
 </script>
 
 <template>
+  <!--子控件-->
+  <Child :count="count" @add="padd"></Child>
   <h1>{{ msg }}</h1>
 
+  <select name="fe" v-model="state.fe">
+    <option value="react">react</option>
+    <option value="vue">vue</option>
+    <option value="test">test</option>
+  </select>
+  <input type="radio" v-model="state.fe" value="react"/>
+  <input type="radio" v-model="state.fe" value="vue"/>
+  <input type="radio" v-model="state.fe" value="test">
+  <p>{{state.fe}}</p>
+  <div>
+    <label>react</label>
+    <input type="checkbox" v-model="state.farray" value="react"/>
+  </div>
+  <div>
+    <label>vue</label>
+    <input type="checkbox" v-model="state.farray" value="vue" />
+  </div>
+  <div>
+    <label>test</label>
+    <input type="checkbox" v-model="state.farray" value="test" />
+  </div>
+  <p>{{state.farray}}</p>
+  <p v-if="state.fe == 'react'">react selected</p>
+  <p v-else-if="state.fe == 'vue'">vue selected</p>
+  <p v-else>test selected</p>
+  <p v-show="state.fe == 'vue'">vue show</p>
+  <div v-for="(item, index) in state.farray" :key="index">
+    <p>item: {{item}}</p>
+  </div>
+  <div v-bind:class="{color:state.isRed}">red </div>
+  <div v-bind:class="[state.color,state.fontsize]">blue</div>
   <div class="card">
     <button type="button" @click="change">count is {{ count }}</button>
     <p>
@@ -30,7 +78,15 @@ function change(){
     <p/>
     <!--更新inner html-->
     <div v-html="htmlcode" ></div>
-    <span>{{oncetest}}</span>
+    <!--只渲染一次-->
+    <span v-once>{{oncetest}}</span>
+    <p/>
+    <input type="text"  v-bind:value="oncetest" >
+    <p/>
+    <input type="text" v-model.lazy="state.modeltext">
+    <!--<input :value="modeltext" @input="modeltext=e.detail.value"/>-->
+    <br/>
+    <p>{{ state.modeltext }}</p>
   </div>
 
   <p>
@@ -50,5 +106,14 @@ function change(){
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.color{
+  background-color: red;
+}
+.blue{
+  color: blue;
+}
+.big{
+  font-size: 20px;
 }
 </style>
