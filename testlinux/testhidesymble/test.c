eabc09a29ce42c849315f4503b43a077bb1b92c9
chenyue0102 <chenyue0102@gmail.com>
2023-10-23 10:05:01 +0800

    test

--- testlinux/testhidesymble/test.c
+++ testlinux/testhidesymble/test.c
@@ -1,8 +1,27 @@
+#include <stdio.h>
+#include <dlfcn.h>
 #include "lib1/call1.h"
 #include "lib2/call2.h"
 
+typedef void (*CALLFUN)(void);
 int main(void){
-	call1();
-	call2();
+	const char *dl1 = "./libso1.so";
+	const char *dl2 = "./libso2.so";
+	void *lib1 = dlopen(dl1, RTLD_NOW);
+	void *lib2 = dlopen(dl2, RTLD_NOW);
+	if (NULL == lib1 || NULL == dl2){
+		printf("dlopen failed\n");
+		return -1;
+	}
+	CALLFUN cal1 = (CALLFUN)dlsym(lib1, "call1");
+	CALLFUN cal2 = (CALLFUN)dlsym(lib2, "call2");
+	if (NULL == cal1 || NULL == cal2){
+		printf("dlsym failed\n");
+		return -1;
+	}
+	cal1();
+	cal2();
+	//call1();
+	//call2();
 	return 0;
 }
